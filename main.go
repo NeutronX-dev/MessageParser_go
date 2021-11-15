bdad48a2477e4433e55a1dd5150c0b1009ade989
Neutron <32855909+NeutronX-dev@users.noreply.github.com>
2021-11-15 08:40:35 -0500

    Removed func main

--- main.go
+++ main.go
@@ -36,19 +36,3 @@ func ParseMessage(prefix, message string) ParsedMessage {
 		return ParsedMessage{}
 	}
 }
-
-func main() {
-	var ToParse string = "!help moderation"
-	message := ParseMessage("!", ToParse)
-	if message.IsParsed {
-		switch message.Command {
-		case "help":
-			if message.HasArguments(1) {
-				fmt.Println(message.Arguments)
-				fmt.Printf("Need help with \"%v\"?\n", strings.Join(message.Arguments, " "))
-			} else {
-				fmt.Println("Need at least one Argument")
-			}
-		}
-	}
-}
